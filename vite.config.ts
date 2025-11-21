95eaaa6a824b9a2198bccae02a3c1a7a3c860569
Kingsword <kingsword09@gmail.com>
2025-11-21 23:22:50 +0800

    chore: Upgrade TailwindCSS and Integrate with Vite Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 const cdn = "https://esm.sh";
@@ -13,7 +14,7 @@ const externalAliases: Record<string, string> = {
 };
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   root: "src/ui",
   build: {
     outDir: "../../dist/ui",
